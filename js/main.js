6a350d0a51a3b4575019ed5b2b8e615e475298b9
asanka9 <sendasanka96@gmail.com>
2020-12-31 16:01:56 +0530

    v2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-var home = document.querySelector(".home");
-var contactUs = document.querySelector(".contactUs");
-var sizeChart = document.querySelector(".sizeChart");
-var ourTeam = document.querySelector(".ourTeam");
 var change_image = document.querySelector(".image-change");
 var index = 0;
 
@@ -18,17 +14,6 @@ window.addEventListener('scroll', function () {
 });
 
 
-/*
-change_image.innerHTML = '<img src="images/bestseller3.jpg" width="100%">';
-index++;
-window.setInterval(function(){
-    var img_list = ['<img src="images/bestseller3.jpg" width="100%">','<img src="images/bestseller2.jpg" width="100%">','<img src="images/bestseller1.jpg" width="100%"></img>'];
-    /// call your function here
-    change_image.innerHTML = img_list[index]
-    index++;
-    index = index%3;
-  }, 3000);
-*/
 
 var myIndex = 0;
 carousel();
@@ -48,34 +33,6 @@ function carousel() {
 
 
 
-function homeClick_(){
-    home.style.display = "Block";
-    contactUs.style.display = "None";
-    sizeChart.style.display = "None";
-    ourTeam.style.display = "None";
-}
-
-function sizeChart_(){
-    home.style.display = "None";
-    contactUs.style.display = "None";
-    sizeChart.style.display = "Block";
-    ourTeam.style.display = "None";
-}
-
-function ourTeam_(){
-    home.style.display = "None";
-    contactUs.style.display = "None";
-    sizeChart.style.display = "None";
-    ourTeam.style.display = "Block";
-}
-
-
-function contactUs_(){
-    home.style.display = "None";
-    contactUs.style.display = "Block";
-    sizeChart.style.display = "None";
-    ourTeam.style.display = "None";
-}
 
 
 
